4b7483890e3830adb0c33993532954cd207e67c2
shubhh015 <shubhashishhhh@gmail.com>
2024-11-16 15:06:04 +0530

    slide increase

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -81,7 +81,7 @@ const Dashboard = () => {
         dots: true,
         infinite: true,
         speed: 500,
-        slidesToShow: 1,
+        slidesToShow: 2,
         slidesToScroll: 1,
         arrows: false,
         responsive: [
